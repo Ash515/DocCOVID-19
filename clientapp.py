9cccb6913df70d0fa11889ce638dca67d53f126d
Ash515 <aaashwin515@gmail.com>
2021-05-01 15:20:58 +0530

    updated new files

--- clientapp.py
+++ clientapp.py
@@ -16,13 +16,22 @@ mysql=MySQL(app)
 @app.route('/')
 def index():
     return render_template('index.html') 
+
 @app.route('/main')
 def main():
-    return render_template('main.html')
+     if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('main.html', useremail=session['user_email'])
+    # User is not loggedin redirect to login page
+     return redirect(url_for('userlogin'))
+
+@app.route('/docmain')
+def docmain():
+    return render_template('docmain.html')
 
 @app.route('/userlog',methods=["GET","POST"])
 def userlogin():
-    msg = ''
+    message = ''
     # Check if "username" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'user_email' in request.form and 'user_psw' in request.form:
         # Create variables for easy access
@@ -43,48 +52,53 @@ def userlogin():
             return redirect(url_for('main'))
         else:
             # Account doesnt exist or username/password incorrect
-            msg = 'Incorrect username/password!'
-    return render_template('userlog.html', msg='')
-   
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            message = 'Incorrect username/password!'
+    return render_template('userlog.html', message='')
    
 @app.route('/userreg',methods=["GET","POST"])
 def userregistration():
-    msg=''
-    if request.method == 'POST':
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'user_name' in request.form and 'user_email' in request.form and 'user_psw' in request.form:
+        # Create variables for easy access
         username = request.form['user_name']
         useremail = request.form['user_email']
-        userpassword=request.form['user_psw']
-        cursor = mysql.connection.cursor()
-        exists=cursor.execute('''SELECT pemail FROM patients WHERE pemail = % s''', (useremail,))
-        if exists:
-            msg="Sorry mail id already exists ..."
+        userpassword = request.form['user_psw']
+     
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM patients WHERE pemail = %s', (useremail,))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', useremail):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not userpassword or not useremail:
+            msg = 'Please fill out the form!'
         else:
-            cursor.execute(''' INSERT INTO patients VALUES(%s,%s,%s)''',(username,useremail,userpassword))
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO patients VALUES (%s, %s, %s)', (username, useremail, userpassword))
             mysql.connection.commit()
-            cursor.close()
-            msg="Successfully registered !"
-    return render_template('userreg.html',msg = msg)
+            msg = 'You have successfully registered!'
+            #after successfully inserted redirect to loginpage
+            return render_template('userlog.html')  
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('userreg.html', msg=msg)
+
+@app.route('/userlogout', methods = ['GET'])
+def userlogout():
+    session.pop('user_email')
+    return redirect(url_for('index'))
+@app.route('/userforgot')
+def userforgot():
+    return render_template('userpassword.html')
+
 
 
 
