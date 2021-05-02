3916b989e98bb22bfe68050c798bae813e0d3b4e
Ash515 <aaashwin515@gmail.com>
2021-05-02 11:41:18 +0530

    updated new files

--- clientapp.py
+++ clientapp.py
@@ -116,64 +116,72 @@ def userforgot():
 
   
 
-'''
+
 @app.route('/doclog',methods=['POST','GET']) 
 def doclog():
-    if 'username' in session:
-        return url_for('docmain')
-    if (request.method=="POST"):
-        docemail=request.form.get('doc_email')
-        docpassword=request.form.get('doc_psw')
-        docemail_found = docemail.find_one({"docemail": docemail})
-        if docemail_found:
-            docemail_val=docemail_found['doc_email']
-            passwordcheck=docemail_found['doc_psw']
-
-            if bcrypt.checkpw(docpassword.encode('utf-8'),passwordcheck):
-                session["docemail"]=docemail_val
-                return redirect(url_for('docmain'))
-            else:
-                if 'docemail' in session:
-                    return redirect(url_for("docmain"))
-                message='Wrong password'
-                return render_template('doclog.html',message=message)
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'doc_email' in request.form and 'doc_psw' in request.form:
+        # Create variables for easy access
+        docemail = request.form['doc_email']
+        docpassword = request.form['doc_psw']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM doctors WHERE demail = %s AND dpassword = %s', (docemail, docpassword))
+        # Fetch one record and return result
+        doctor= cursor.fetchone()
+                # If account exists in patients table in out database
+        if doctor:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['doc_psw'] = doctor['dpassword']
+            session['doc_email'] = doctor['demail']
+            # Redirect to home page
+            return redirect(url_for('docmain'))
         else:
-            message="email not found"
-            return render_template('userlog.html', message=message)
-    return render_template('doclog.html')
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    return render_template('doclog.html', msg='')
 
 @app.route('/docreg',methods=["GET","POST"])
-def docreg():
-    if request.method == 'POST':
-        docuname = doctors.find_one({'docuname' : request.form['doc_uname']})
-        docemail = doctors.find_one({'docemail' : request.form['doc_email']})
-        if docuname is None:
-            hashpass = bcrypt.hashpw(request.form['doc_psw'].encode('utf-8'), bcrypt.gensalt())
-            doctors.insert({'docuname' : request.form['doc_uname'], 'doc_password' : hashpass,'docemail':docemail})
-            session['docuname'] = request.form['docuname']
-            return render_template('doclog.html')
-        return 'That username already exists!'
-
-    return render_template('docreg.html')
-'''
-            
-
-
-
-
-
-    
-
-
-
-    
-
+def doctorregistration():
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'doc_uname' in request.form and 'doc_email' in request.form and 'doc_psw' in request.form and 'doc_conpsw' in request.form:
+        # Create variables for easy access
+        docname = request.form['doc_uname']
+        docemail = request.form['doc_email']
+        docpassword = request.form['doc_psw']
+        docconpassword = request.form['doc_conpsw']
+     
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM doctors WHERE demail = %s', (docemail,))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+',docemail):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', docname):
+            msg = 'Username must contain only characters and numbers!'
+        elif docpassword!=docconpassword:
+            msg="Password mismatch with confirm password"
+        elif not docname or not docpassword or not docemail:
+            msg = 'Please fill out the form!'
+        else:
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO doctors VALUES (%s, %s, %s)', (docname, docemail, docpassword))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+            #after successfully inserted redirect to loginpage
+            return render_template('doclog.html')  
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('docreg.html', msg=msg)
 
 if __name__=='__main__':
-   
-   
-
-   
-
     app.debug = True
     app.run()
